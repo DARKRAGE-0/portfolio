75a412fe9cdabbde095ba61847ed8a32cb9ea837
darkrage <darkrage6162@gmail.com>
2021-05-06 05:38:46 -0700

    project showcase added.

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".navBar").style.top = "0";
-  } else {
-    document.querySelector(".navBar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+  	var currentScrollPos = window.pageYOffset;
+  	if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navBar").style.top = "0";}
+	else{document.querySelector(".navBar").style.top = "-50px";}
+  	prevScrollpos = currentScrollPos;
+}
+
+
+// PROJECT SHOWCASE ON HOME-PAGE
+let slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {showSlides(slideIndex += n);}
+// Thumbnail image controls
+function currentSlide(n) {showSlides(slideIndex = n);}
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");let i;
+  let captionText = document.getElementById("caption");
+  if (n>slides.length){slideIndex=1}
+  if (n<1){slideIndex=slides.length}
+  for (i=0;i<slides.length;i++){slides[i].style.display="none";}
+  for (i=0;i<dots.length;i++){
+  dots[i].className=dots[i].className.replace(" active", "");}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
