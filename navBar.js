67f5521b884a2c9b3dbe347d9af49b19fc377978
darkrage <darkrage6162@gmail.com>
2021-05-07 15:47:49 -0700

    navBar is now a component.

---
+++ navBar.js
@@ -0,0 +1,8 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  	var currentScrollPos = window.pageYOffset;
+  	if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navBar").style.top = "0";}
+	else{document.querySelector(".navBar").style.top = "-50px";}
+  	prevScrollpos = currentScrollPos;
+}
--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  	var currentScrollPos = window.pageYOffset;
-  	if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".navBar").style.top = "0";}
-	else{document.querySelector(".navBar").style.top = "-50px";}
-  	prevScrollpos = currentScrollPos;
-}
-
-
 // PROJECT SHOWCASE ON HOME-PAGE
 let slideIndex = 1;
 showSlides(slideIndex);
